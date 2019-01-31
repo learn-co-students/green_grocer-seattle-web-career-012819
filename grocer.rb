a2a8787baaae904583d78e4c5e8615fcb153a006
Emily Wagner <emilywagner598@gmail.com>
2019-01-31 23:55:13 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,56 @@
+require "pry"
+
 def consolidate_cart(cart)
-  # code here
+  new_hash = {}
+
+  cart.each do |element|
+    if new_hash.keys.include?(element.keys.first) == false
+      element[element.keys.first][:count] = 1
+      new_hash[element.keys.first] = element[element.keys.first]
+    else
+      new_hash[element.keys.first][:count] += 1
+    end
+  end
+  new_hash
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  new_hash = {}
+
+  if coupons.size == 0
+    return cart
+  end
+
+  cart.each do |k, v|
+    coupon = coupons.find { |element| element[:item] == k}
+
+    new_hash[k] = v
+
+    if coupon != nil
+      new_hash[k + " W/COUPON"] = {price: coupon[:cost], clearance: v[:clearance], count: (v[:count] / coupon[:num])}
+      new_hash[k][:count] -= (coupon[:num] * new_hash[k + " W/COUPON"][:count])
+    end
+  end
+  new_hash
 end
 
 def apply_clearance(cart)
-  # code here
+  cart.each do |k,v|
+    if v[:clearance] == true
+      v[:price] -= (v[:price]*0.2)
+    end
+  end
 end
 
 def checkout(cart, coupons)
-  # code here
+  total = 0.0
+  apply_clearance(apply_coupons(consolidate_cart(cart), coupons)).each do |k, v|
+      total += (v[:price] * v[:count])
+  end
+
+  if total > 100
+    total -= total*0.1
+  end
+
+  total
 end
